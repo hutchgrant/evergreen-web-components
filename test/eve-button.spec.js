f4d4df5031e2af18c42e2fa177c84ec763e0ef0c
HutchGrant <h.g.utchinson@gmail.com>
2019-01-07 01:44:26 -0500

    Adding circleci, updating tests

---
+++ test/eve-button.spec.js
@@ -0,0 +1,74 @@
+import { html } from '@polymer/lit-element';
+import { render } from 'lit-html';
+import '../packages/button/src/eve-button.js';
+
+describe('Button Component', () => {
+
+  describe('Passing multiple attributes', () => {
+    let button, testBed, template;
+    const testSize = 'md';
+    const url = 'https://www.google.com/';
+
+    const handleClick = () => {
+      console.log('button clicked!');
+    };
+
+    beforeEach(async () => {
+      testBed = document.createElement('div');
+      template = html`
+        <eve-button size="${testSize}" .onClick="${handleClick}">My Button</eve-button>
+      `;
+
+      render(template, testBed);
+      document.body.appendChild(testBed);
+      button = testBed.firstElementChild;
+
+      await button.updateComplete;
+    });
+
+    afterEach(() => {
+      button.remove();
+      button = null;
+    });
+
+    it('should display the slot text', () => {
+      const testButton = button.shadowRoot.querySelector('slot').assignedNodes()[0];
+
+      expect(testButton.textContent).toEqual('My Button');
+    });
+
+    it('should have the correct width', () => {
+      const testButton = button.shadowRoot.querySelector('.btn');
+
+      const size = 'btn-' + testSize;
+
+      expect(testButton).toHaveClass(size);
+    });
+
+    it('should add a click handler', () => {
+      const testButton = button.shadowRoot.querySelector('.btn');
+      const spyEvent = spyOn(testButton, 'click');
+
+      testButton.click();
+      expect(spyEvent).toHaveBeenCalled();
+    });
+
+    it('should have href when attribute is present', async () => {
+      button.remove();
+      button = null;
+
+      template = html`
+        <eve-button size="${testSize}" href="${url}">My Button</eve-button>
+      `;
+      render(template, testBed);
+      document.body.appendChild(testBed);
+      button = testBed.firstElementChild;
+
+      await button.updateComplete;
+
+      const testButton = button.shadowRoot.querySelector('.btn');
+
+      expect(testButton.href).toEqual('https://www.google.com/');
+    });
+  });
+});
